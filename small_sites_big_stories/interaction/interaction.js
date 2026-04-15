7030d794f2e0116e70c0066bd5f073abd20990fb
snails91 <sasharoselee@gmail.com>
2026-04-15 19:29:58 -0400

    loop

--- small_sites_big_stories/interaction/interaction.js
+++ small_sites_big_stories/interaction/interaction.js
@@ -1,1 +1,22 @@
 console.log("This is the interaction site!");
+	
+		// Target an element (in this case, by its tag) and assign it to a variable
+		let body = document.querySelector('body');
+
+		// Create a function
+		function goRando() {
+
+			// Generate a random number between 0 and 1 and assign it to a variable
+			let randomNumber = Math.random();
+
+			// Check if the number’s value is less than 0.3
+			if (randomNumber < 0.3) {
+				// Change the body’s background color
+				body.style.backgroundColor = 'blue';
+			} else if (randomNumber < 0.6) {
+				body.style.backgroundColor = 'red';
+			} else {
+				body.style.backgroundColor = 'yellow';
+			}
+		}
+	
