74257e89dbc11161b344a501d2db415ab1ed3a29
snails91 <sasharoselee@gmail.com>
2026-04-15 11:53:22 -0400

    week7

--- small_sites_big_stories/loop/loop.js
+++ small_sites_big_stories/loop/loop.js
@@ -1,1 +1,21 @@
-console.log("This is the loop site!");
+/* console.log("This is the loop site!");
+	let container = document.querySelector('.container');
+
+		for (let i=0; i<100; i++) {
+			let randomWidth = Math.random()*100;
+			let randomHeight = Math.random()*100;
+			let newDiv = `<div style="width: ${randomWidth}px; height: ${randomHeight}px;"></div>`;
+			container.innerHTML += newDiv;
+		}
+
+		function changeSizes() {
+			let divs = document.querySelectorAll('.container div');
+			divs.forEach((item) => {
+				let randomWidth = Math.random()*100;
+				let randomHeight = Math.random()*100;
+				item.style.width = randomWidth + "px";
+				item.style.height = randomHeight + "px";
+			})
+		}
+
+		setInterval(changeSizes, 500);*/
